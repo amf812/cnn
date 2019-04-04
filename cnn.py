4670e7850937c7d7f7b3a2f1b10e471dbd8b1c41
unknown <fountaia@mail.gvsu.edu>
2019-04-04 17:15:00 -0400

    Updated

--- cnn.py
+++ cnn.py
@@ -30,11 +30,11 @@ shear_range = 0.2,
 zoom_range = 0.2,
 horizontal_flip = True)
 test_datagen = ImageDataGenerator(rescale = 1./255)
-training_set = train_datagen.flow_from_directory('training_set',
+training_set = train_datagen.flow_from_directory('training',
 target_size = (64, 64),
 batch_size = 32,
 class_mode = 'binary')
-test_set = test_datagen.flow_from_directory('test_set',
+test_set = test_datagen.flow_from_directory('test',
 target_size = (64, 64),
 batch_size = 32,
 class_mode = 'binary')
